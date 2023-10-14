2cbaa187ddf7c3a3b3c45ca4fe419d3d7b52396b
Jumper-44 <89034685+Jumper-44@users.noreply.github.com>
2023-10-14 13:57:40 +0200

    renamed file

--- JL_general.lua
+++ JL_general.lua
@@ -1,6 +1,9 @@
 -- Author: Jumper
 -- GitHub: https://github.com/Jumper-44
 
+---@section tau
+tau = math.pi*2
+---@endsection
 
 ---@section clamp
 ---Clamp the value x in the range [s;l]
@@ -10,5 +13,13 @@
 ---@return number
 clamp = function(x,s,l)
     return x < s and s or x > l and l or x
+end
+---@endsection
+
+---@section getNumber
+getNumber = function(...)
+    local r = {...}
+    for i = 1, #r do r[i] = input.getNumber(r[i]) end
+    return table.unpack(r)
 end
 ---@endsection
