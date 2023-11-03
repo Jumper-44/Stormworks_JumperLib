44466ba1ad5a1196146b7f65df6305436dbf7212
Jumper-44 <89034685+Jumper-44@users.noreply.github.com>
2023-11-04 00:54:22 +0100

    "matrices are read column-major" comment

--- Math/JL_matrix_operations.lua
+++ Math/JL_matrix_operations.lua
@@ -4,7 +4,9 @@
 
 require('JumperLib.Math.JL_vector_operations')
 
----matrices are read by m[column][row]
+---matrices are read column-major order like: m[column][row]  
+---m[1][1], m[2][1], m[3][1]  
+---m[1][2], m[2][2], m[3][2]
 ---@alias matrix table<number, table<number, number>>
 ---@alias matrix2x2 matrix
 ---@alias matrix3x3 matrix
