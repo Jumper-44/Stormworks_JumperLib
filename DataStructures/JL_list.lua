dc495c8ec2aa45a862dc02d27c98a75dc8b744fb
Jumper-44 <89034685+Jumper-44@users.noreply.github.com>
2023-11-04 14:27:37 +0100

    insert & remove won't be minified so added prefix

--- DataStructures/JL_list.lua
+++ DataStructures/JL_list.lua
@@ -4,7 +4,7 @@
 
 ---@section list
 ---Table/list in which removed entries are marked for removal and overwritten by new inserted values.  
----Purpose for changing the format from {{a,b,c}, {a,b,c}, ...} to {{a ,a, ...}, {b, b, ...}, {c, c, ...}}, which reduces tables
+---Purpose for changing the table structure from {{a,b,c}, {a,b,c}, ...} to {{a ,a, ...}, {b, b, ...}, {c, c, ...}}, which reduces tables
 ---@param elements table -- expects: {{}, {}, ..., {}}
 ---@param removed_id nil -- dirty local to reduce char
 ---@param id nil -- dirty local to reduce char
@@ -12,26 +12,31 @@
 ---@return list
 function list(elements, removed_id, id)
     ---@class list
-    ---@field insert fun(new_elements: table):integer
-    ---@field remove fun(index: integer)
+    ---@field list_insert fun(new_elements: table):integer
+    ---@field list_remove fun(index: integer)
 
     removed_id = {}
 
-    ---@param new_elements table new_elements[1] must not be nil
+    ---@section list_insert
+    ---@param new_elements table new_elements[1] must not be nil, as it relies on #elements[1] to get length
     ---@return integer element_id returns the given id to new_elements
-    elements.insert = function(new_elements)
+    function elements.list_insert (new_elements)
         id = #removed_id > 0 and table.remove(removed_id) or #elements[1]+1
         for i = 1, #elements do
             elements[i][id] = new_elements[i]
         end
         return id
     end
+    ---@endsection
 
+    ---@section list_remove
     ---Assumes removed index is in range
     ---@param index integer
-    elements.remove = function(index)
+    function elements.list_remove(index)
         removed_id[#removed_id+1] = index
     end
+    ---@endsection
+
     return elements
 end
 ---@endsection
@@ -47,24 +52,24 @@ do
     local t1, t2, t3, t4
 
     t1 = os.clock()
-    for i = 1, 1e7 do
+    for i = 1, 5e6 do
         buffer[1] = i
         buffer[2] = i % 3
         buffer[3] = i % 2 == 0
 
-        arr.insert(buffer)
+        arr.list_insert(buffer)
     end
     t2 = os.clock()
-    for i = 1, 1e6 do
-        arr.remove(i)
+    for i = 1, 5e6 do
+        arr.list_remove(i)
     end
     t3 = os.clock()
-    for i = 1, 1e7 do
+    for i = 1, 5e6 do
         buffer[1] = i
         buffer[2] = i % 5
         buffer[3] = i % 3 == 0
 
-        arr.insert(buffer)
+        arr.list_insert(buffer)
     end
     t4 = os.clock()
 
